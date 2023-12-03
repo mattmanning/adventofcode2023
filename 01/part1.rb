e4106448710dbcc43d61ba0395fc5a2893367236
Matthew Manning <matt@Matthews-MacBook-Pro.local>
2023-12-02 21:44:42 -0500

    fix indentation

--- 01/part1.rb
+++ 01/part1.rb
@@ -1,6 +1,6 @@
 total = File.foreach('input').inject(0) do |sum, line|
-	str = line.delete('^0-9')
-	sum + "#{str[0]}#{str[-1]}".to_i
+  str = line.delete('^0-9')
+  sum + "#{str[0]}#{str[-1]}".to_i
 end
 
 puts total
--- 01/part2.rb
+++ 01/part2.rb
@@ -1,44 +1,44 @@
 require 'strscan'
 
 def numberize(s)
-	case s
-	when 'one'
-		1
-	when 'two'
-		2
-	when 'three'
-		3
-	when 'four'
-		4
-	when 'five'
-		5
-	when 'six'
-		6
-	when 'seven'
-		7
-	when 'eight'
-		8
-	when 'nine'
-		9
-	else
-		s.to_i
-	end
+  case s
+  when 'one'
+    1
+  when 'two'
+    2
+  when 'three'
+    3
+  when 'four'
+    4
+  when 'five'
+    5
+  when 'six'
+    6
+  when 'seven'
+    7
+  when 'eight'
+    8
+  when 'nine'
+    9
+  else
+    s.to_i
+  end
 end
 
 total = File.foreach('input').inject(0) do |sum, line|
-	arr = []
-	s = StringScanner.new(line)
-	while !s.eos? do
-		r = s.scan /[0-9]|one|two|three|four|five|six|seven|eight|nine|ten/
-		if r.nil?
-			s.pos = s.pos + 1
-		else
-			arr.push r
-			s.pos = s.pos - 1 unless r.length == 1
-		end
-	end
-	n = "#{numberize(arr[0])}#{numberize(arr[-1])}".to_i
-	sum + n
+  arr = []
+  s = StringScanner.new(line)
+  while !s.eos? do
+    r = s.scan /[0-9]|one|two|three|four|five|six|seven|eight|nine|ten/
+    if r.nil?
+      s.pos = s.pos + 1
+    else
+      arr.push r
+      s.pos = s.pos - 1 unless r.length == 1
+    end
+  end
+  n = "#{numberize(arr[0])}#{numberize(arr[-1])}".to_i
+  sum + n
 end
 
 puts total
